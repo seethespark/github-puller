67547277804f44a5677b0947f64db34e20b122a7
Nick Whiteley <nick@seethespark.com>
2015-08-31 11:46:02 +0100

    Added files

---
+++ app.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var gitHubWebhookHandler = require('github-webhook-handler');
+var GitHubApi = require("github");
+
+var handler = gitHubWebhookHandler({ path: '/i-flicks', secret: 'lorcanvida' });
+
+var gitHubApi = new GitHubApi({
+    // required
+    version: "3.0.0",
+    // optional
+    debug: true,
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    timeout: 5000,
+    headers: {
+        "user-agent": "Nicks-GitHub-App" // GitHub is happy with a unique user agent
+    }
+});
+
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 404;
+        res.end('no such location');
+    });
+}).listen(7777);
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message);
+});
+
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref);
+    /// get https://github.com/seethespark/i-flicks/archive/master.zip
+});
+
+handler.on('issues', function (event) {
+    console.log('Received an issue event for %s action=%s: #%d %s',
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title);
+});
