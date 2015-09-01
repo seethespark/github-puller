bc61e95d8b9ac9fd0c44cf60fc92c84c9b26e631
Nick Whiteley <nick@seethespark.com>
2015-09-01 15:12:47 +0100

    add hook paths as setting and thenloop through

--- app.js
+++ app.js
@@ -4,31 +4,14 @@ var fs = require('filesystem');
 var gitHubWebhookHandler = require('github-webhook-handler');
 var settings = {};
 settings.localPath = 'c:\\blaa';
+settings.hooks = [{name: 'gitHubPuller', localPath: ''}];
 
-var iFLicksHandler = gitHubWebhookHandler({ path: '/i-flicks', secret: 'lorcanvida' });
-var gitHubPullerHandler = gitHubWebhookHandler({ path: '/gitHubPuller', secret: 'lorcanvida' });
-
-http.createServer(function (req, res) {
-    gitHubPullerHandler(req, res, function (err) {
-        res.statusCode = 404;
-        res.end('no such location');
-    });
-}).listen(7777);
-function errorHandler(err, location, res) {
-    console.log('Error at ', location, '.', 'Message: ', err.message);
-    if (res) {
-        res.status(500);
-        res.end();
-    }
-}
-gitHubPullerHandler.on('error', function (err) {
-    console.error('Error:', err.message);
-});
-
-gitHubPullerHandler.on('push', function (event) {
-	var added =event.payload.commits.added,
-	    removed =event.payload.commits.removed,
-	    changed =event.payload.commits.changed,
+for (var i = 0; i < settings.hooks.length; i++) {
+	var handler = gitHubWebhookHandler({ path: settings.hooks[i].name, secret: 'lorcanvida' });
+	handler.on('push', function (event) {
+		var added =event.payload.head_commit.added,
+	    removed =event.payload.head_commit.removed,
+	    modified =event.payload.head_commit.modified,
 	    remotePath =event.payload.url,
 	    i;
 	
@@ -40,19 +23,42 @@ gitHubPullerHandler.on('push', function (event) {
             }
 		}
 	}
-	
-	
-	
+
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref);
     /// get https://github.com/seethespark/i-flicks/archive/master.zip
-});
+	});
+    handler.on('error', function (err) {
+        console.error('Error:', err.message);
+    });
 
+
+    settings.hooks[i].handler = handler;
+}
+http.createServer(function (req, res) {
+	for (var i = 0; i < settings.hooks.length; i++) {
+		settings.hooks[i].handler(req, res, function (err) {
+	        res.statusCode = 404;
+	        res.end('no such location');
+	    });
+	}
+}).listen(7777);
+
+function errorHandler(err, location, res) {
+    console.log('Error at ', location, '.', 'Message: ', err.message);
+    if (res) {
+        res.status(500);
+        res.end();
+    }
+}
+
+
+/*
 gitHubPullerHandler.on('issues', function (event) {
     console.log('Received an issue event for %s action=%s: #%d %s',
         event.payload.repository.name,
         event.payload.action,
         event.payload.issue.number,
         event.payload.issue.title);
-});
+});*/
