a285846425bff269c110c272947901913988860f
Nick Whiteley <nick@nickwhiteley.com>
2015-09-02 13:48:06 +0100

    Linted

--- app.js
+++ app.js
@@ -8,32 +8,30 @@ var settings = {};
 settings.hooks = [{name: '/gitHubPuller', localPath: '/var/www/gitHubPuller'}];
 
 for (var i = 0; i < settings.hooks.length; i++) {
-	var handler = gitHubWebhookHandler({ path: settings.hooks[i].name, secret: 'lorcanvida' });
-	handler.on('push', function (event) {
-		var added = event.payload.head_commit.added,
-	        removed = event.payload.head_commit.removed,
-	        modified = event.payload.head_commit.modified,
-	        remotePath = event.payload.head_commit.url,
-	        j;
-	
-	    for (j = 0; j < modified.length; j++) {
-		console.log(remotePath + '/' + modified[j]);
-		    https.get(remotePath + '/' + modified[j], function(res) {
-			    
-			    fs.write(path.join(settings.hooks[i].localPath, modified[j]), res.body, function(err) {
+    var handler = gitHubWebhookHandler({ path: settings.hooks[i].name, secret: 'lorcanvida' });
+    handler.on('push', function (event) {
+        var added = event.payload.head_commit.added,
+            removed = event.payload.head_commit.removed,
+            modified = event.payload.head_commit.modified,
+            remotePath = event.payload.head_commit.url,
+            j;
+        for (j = 0; j < modified.length; j++) {
+            console.log(settings.hooks[i]);
+            https.get(remotePath + '/' + modified[j], function(res) {
+                fs.write(path.join(settings.hooks[i].localPath, modified[j]), res.body, function(err) {
                     if (err) { errorHandler(err, 'push2'); return; }
                 });
-		    }).on('error', function(err) {
+            })
+            .on('error', function(err) {
                 if (err) { errorHandler(err, 'push1'); return; }
             });
-	    }
-
+        }
 
         console.log('Received a push event for %s to %s',
             event.payload.repository.name,
             event.payload.ref);
         /// get https://github.com/seethespark/i-flicks/archive/master.zip
-	    });
+    });
     handler.on('error', function (err) {
         console.error('Error:', err.message);
     });
@@ -42,19 +40,19 @@ for (var i = 0; i < settings.hooks.length; i++) {
 }
 
 http.createServer(function (req, res) {
-	for (var i = 0; i < settings.hooks.length; i++) {
-		if (req.url === settings.hooks[i].name) {
+    for (var i = 0; i < settings.hooks.length; i++) {
+        if (req.url === settings.hooks[i].name) {
              settings.hooks[i].handler(req, res);
              return;
-         }
-	}
-	res.statusCode = 404;
+        }
+    }
+    res.statusCode = 404;
     res.end('no such location');
 }).listen(7777);
 
 function errorHandler(err, location, res) {
-	var message = err;
-	if (err.message) {
+    var message = err;
+    if (err.message) {
         message = err.message;
     }
     console.log('Error at ', location, '.', 'Message: ', message);
