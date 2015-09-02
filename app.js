5d826f26642d2f8127d959e88da5f146abfa5c59
Nick Whiteley <nick@nickwhiteley.com>
2015-09-02 13:55:14 +0100

    Add test console.log

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ for (var i = 0; i < settings.hooks.length; i++) {
             modified = event.payload.head_commit.modified,
             remotePath = event.payload.head_commit.url,
             j;
+        console.log(localPath);
         for (j = 0; j < modified.length; j++) {
+            console.log(modified[j]);
             https.get(remotePath + '/' + modified[j], function(res) {
                 fs.write(path.join(localPath, modified[j]), res.body, function(err) {
                     if (err) { errorHandler(err, 'push2'); return; }
