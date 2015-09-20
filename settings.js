04cf1416933d91a23f960b5f4278ca3304b7f3ab
Nick Whiteley <nick@nickwhiteley.com>
2015-09-20 21:28:08 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,12 @@
+var settings = {};
+settings.hooks = [{name: '/gitHub name',
+    localPath: '/local path if storing locally',
+    sftpPath: '/path on sftp server',
+    sftp: {
+        username: 'my username',
+        password: 'my password',
+        host: 'sftp server IP or DNS name',
+        }
+    }];
+
+module.exports = settings;
